1a20f32efd452fa0915b249b092400596041de21
LauranRares <106803768+LauranRares@users.noreply.github.com>
2022-11-29 18:40:37 +0200

    Add files via upload

---
+++ Models/TheShoppingCart.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TMS.Models
+{
+    public class TheShoppingCart
+    {
+        public int Id { get; set; }
+
+        public string TheUserId { get; set; }
+
+        [ForeignKey("TheUserId")]
+        public UserDB TheUser { get; set; }
+
+        public int BookId { get; set; }
+
+        public string Title { get; set; }
+
+        public string Author { get; set; }
+
+        public int Quantity { get; set; }
+
+        public double Price { get; set; }
+    }
+}
---
+++ Models/UserDB.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace TMS.Models
+{
+    public class UserDB : IdentityUser
+    {
+        public string Name { get; set; }
+
+        public string Location { get; set; }
+
+        public string Address { get; set; }
+
+        public string PhoneNr { get; set; }
+
+        public string CreditCard { get; set; }
+
+        public string Pet { get; set; }
+    }
+}
